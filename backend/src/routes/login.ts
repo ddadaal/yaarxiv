460585dc9cd19ad1949f6e77d9fac7f2cc2bc69e
Chen Junda <ddadaal@outlook.com>
2020-07-31 14:50:54 +0800

    use hapi like route for better clarity

--- backend/src/routes/login.ts
+++ backend/src/routes/login.ts
@@ -4,25 +4,28 @@ import Schema from "schemas/login.route.json";
 import { Querystring, SuccessResponse, FailedResponse } from "types/login.route";
 
 async function loginRoutes(fastify: FastifyInstance) {
-  fastify.get<{
+  fastify.route<{
     Querystring: Querystring,
-  }>("/login", {
+  }>({
+    method: "GET",
+    url: "/login",
     schema: {
       description: "Login",
       querystring: Schema.definitions.Querystring,
       response: {
         200: Schema.definitions.SuccessResponse,
-        401: Schema.definitions.FailedResponse,
+        403: Schema.definitions.FailedResponse,
       },
     },
-  }, async (req, reply): Promise<SuccessResponse | FailedResponse> => {
-    const { username, password } = req.query;
-    if (username === password) {
-      return { token:  username };
-    } else {
-      reply.statusCode = 403;
-      return { reason: 403 };
-    }
+    handler: async (req, reply): Promise<SuccessResponse | FailedResponse> => {
+      const { username, password } = req.query;
+      if (username === password) {
+        return { token:  username };
+      } else {
+        reply.statusCode = 403;
+        return { reason: 403 };
+      }
+    },
   });
 }
 
