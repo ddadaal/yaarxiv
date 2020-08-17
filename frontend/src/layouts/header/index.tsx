0200662ee2eae3e33281a865c1606106c15e8e84
Chen Junda <ddadaal@outlook.com>
2020-08-17 15:16:51 +0800

    clean up logo

--- frontend/src/layouts/header/index.tsx
+++ frontend/src/layouts/header/index.tsx
@@ -5,16 +5,14 @@ import { LanguageSelector } from "./LanguageSelector";
 import { UserIndicator } from "./UserIndicator";
 import { AnchorLink } from "src/components/AnchorLink";
 
-const gravatarSrc =
-  "//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80";
 
 export const Header: React.FC = (props) => {
   return (
     <GrommetHeader background="dark-1" pad="small">
       <Box direction="row" align="center" gap="small">
-        <Avatar src={gravatarSrc} />
+        {/* <Avatar src={gravatarSrc} /> */}
         <AnchorLink color="white" href="/">
-          ShimiSun
+          yaarxiv
         </AnchorLink>
       </Box>
       <Box direction="row" align="center" gap="small">
