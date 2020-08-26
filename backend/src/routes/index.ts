d55486592b87a3e34217ff212ef66f8637d79502
Chen Junda <ddadaal@outlook.com>
2020-08-26 16:57:56 +0800

    clean code

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,7 @@
-import * as home from "./home";
 import * as login from "./auth";
 import * as article from "./article";
 
 export const routes = [
-  home.homeRoutes,
   login.authRoutes,
   article.articlesRoutes,
 ];
