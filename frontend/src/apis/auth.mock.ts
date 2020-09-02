b70fb15e75a170a8c280f195a976e76ade7242cd
Chen Junda <ddadaal@outlook.com>
2020-09-02 09:35:58 +0800

    fix build error

--- frontend/src/apis/auth.mock.ts
+++ frontend/src/apis/auth.mock.ts
@@ -4,10 +4,10 @@ import type { authApis } from "./auth";
 export const authApisMock: MockApi<typeof authApis> = (({ makeHttpError }) => ({
   login: async ({ query: { id, password } }) => {
     if (password === "admin") {
-      return { token: "admin" ,name: "admin1", role: "admin" };
+      return { token: "admin" ,name: "admin1", role: "admin", userId: "admin1" };
     }
     if (password === "1") {
-      return { token: id, name: "cjd", role: "user" };
+      return { token: id, name: "cjd", role: "user", userId: "1" };
     } else {
       throw makeHttpError({}, 401);
     }
@@ -16,7 +16,7 @@ export const authApisMock: MockApi<typeof authApis> = (({ makeHttpError }) => ({
     if (email === "c@c.com") {
       throw makeHttpError({}, 405);
     } else {
-      return { token: email, name: email.split("@")[0] };
+      return { token: email, name: email.split("@")[0], userId: email };
     }
   },
 
