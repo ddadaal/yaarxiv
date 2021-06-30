16354231694e8343a4edc11b7ffb6867160ce1a5
ddadaal <ddadaal@outlook.com>
2021-06-30 18:22:31 +0800

    fix dashboard apis

--- backend/src/utils/pagination.ts
+++ backend/src/utils/pagination.ts
@@ -3,5 +3,5 @@ import { config } from "./config";
 
 export const paginationProps = (page?: number) => ({
   offset: ((page ?? 1) - 1) * config.defaultPageSize,
-  take: config.defaultPageSize,
+  limit: config.defaultPageSize,
 });
