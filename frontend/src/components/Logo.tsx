e0e204adcd80100f5458286c6b2b4929e95cfd50
Chen Junda <ddadaal@outlook.com>
2021-07-03 14:35:04 +0800

    fix header logo

--- frontend/src/components/Logo.tsx
+++ frontend/src/components/Logo.tsx
@@ -1,14 +1,15 @@
 import React, { CSSProperties } from "react";
 import { Image } from "grommet";
+import logo from "src/assets/logo.svg";
 
 interface Props {
   style?: CSSProperties;
 }
 
-export const Logo: React.FC<Props> = (props)=> (
+export const Logo: React.FC<Props> = ()=> (
   <Image
     fit="contain"
-    src="logo.svg"
+    src={logo}
     title="logo"
   />
 );
