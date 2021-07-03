a7f29405e18bdf72d9d31f3475fd54252a5f560a
Chen Junda <ddadaal@outlook.com>
2021-07-03 10:36:05 +0800

    fix development

--- frontend/next-env.d.ts
+++ frontend/next-env.d.ts
@@ -1,14 +1,3 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
-
-declare global {
-  namespace NodeJS {
-    interface ProcessEnv {
-      NODE_ENV: "development" | "production";
-      NEXT_PUBLIC_API_ROOT: string;
-      NEXT_PUBLIC_STATIC_ROOT: string;
-      NEXT_PUBLIC_USE_MOCK: "1" | "0";
-    }
-  }
-}
---
+++ frontend/src/types/env.d.ts
@@ -0,0 +1,10 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: "development" | "production";
+      NEXT_PUBLIC_API_ROOT: string;
+      NEXT_PUBLIC_STATIC_ROOT: string;
+      NEXT_PUBLIC_USE_MOCK: "1" | "0";
+    }
+  }
+}
