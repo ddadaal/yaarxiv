005e7460d2e88aa7e5929ef4b86a3cc06cbcc7b0
Chen Junda <ddadaal@outlook.com>
2021-07-08 15:40:56 +0800

    remove redis plugin for now

--- backend/src/plugins/index.ts
+++ backend/src/plugins/index.ts
@@ -2,7 +2,6 @@ import fastifyCorsPlugin from "fastify-cors";
 import { jwtAuthPlugin } from "./auth";
 import { mailPlugin } from "./mail";
 import { ormPlugin } from "./orm";
-import { redisPlugin } from "./redis";
 import { staticPlugin } from "./static";
 import { swaggerPlugin } from "./swagger";
 import { uploadPlugin } from "./upload";
@@ -15,5 +14,4 @@ export const plugins = [
   ormPlugin,
   mailPlugin,
   jwtAuthPlugin,
-  // redisPlugin,
 ];
