efd37419650dddcfb1b5f458270b6f2317e11d5a
Chen Junda <ddadaal@outlook.com>
2021-07-29 09:31:43 +0800

    frontend: adjust dir structure

--- frontend/src/pages/dashboard/changePassword.tsx
+++ frontend/src/pages/dashboard/changePassword.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { prefix } from "src/i18n";
 import { DashboardLayout } from "src/pageComponents/Dashboard/DashboardLayout";
 import { SectionTitle } from "src/pageComponents/Dashboard/SectionTitle";
-import { ChangePasswordForm } from "src/pageComponents/Dashboard/UserProfile/ChangePasswordForm";
+import { ChangePasswordForm } from "src/pageComponents/Dashboard/ChangePasswordForm";
 
 const root = prefix("pages.dashboard.");
 
